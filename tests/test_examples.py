0eb1690821867ae767c055fba6da55cc2183a7bf
Tom Gustafsson <jkinnala@gmail.com>
2018-07-12 23:30:38 +0300

    Fixed the tests related to examples.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,37 +7,38 @@ These tests run the examples and check that their output stays constant.
 """
 
 class TestEx1(unittest.TestCase):
-    """Run examples/ex1.py"""
+    """Run examples/ex01.py"""
     def runTest(self):
-        import examples.ex1
-        self.assertAlmostEqual(np.max(examples.ex1.x), 0.07344576657)
+        import examples.ex01
+        self.assertAlmostEqual(np.max(examples.ex01.x), 0.07344576657)
 
 class TestEx3(unittest.TestCase):
-    """Run examples/ex3.py"""
+    """Run examples/ex03.py"""
     def runTest(self):
-        import examples.ex3
-        self.assertAlmostEqual(examples.ex3.L[0], 0.00418289)
+        import examples.ex03
+        self.assertAlmostEqual(examples.ex03.L[0], 0.00418289)
 
 class TestEx5(unittest.TestCase):
-    """Run examples/ex5.py"""
+    """Run examples/ex05.py"""
     def runTest(self):
-        import examples.ex5
-        self.assertAlmostEqual(np.max(examples.ex5.x), 0.93570751751091152)
+        import examples.ex05
+        self.assertAlmostEqual(np.max(examples.ex05.x), 0.93570751751091152)
 
 class TestEx6(unittest.TestCase):
-    """Run examples/ex6.py"""
+    """Run examples/ex06.py"""
     def runTest(self):
-        import examples.ex6
-        self.assertAlmostEqual(np.max(examples.ex6.x), 0.073651530833125131)
+        import examples.ex06
+        self.assertAlmostEqual(np.max(examples.ex06.x), 0.073651530833125131)
 
 class TestEx7(unittest.TestCase):
-    """Run examples/ex7.py"""
+    """Run examples/ex07.py"""
     def runTest(self):
-        import examples.ex7
-        self.assertAlmostEqual(np.max(examples.ex7.x), 0.077891428529719878)
+        import examples.ex07
+        self.assertAlmostEqual(np.max(examples.ex07.x), 0.077891428529719878)
 
 class TestEx9(unittest.TestCase):
-    """Run examples/ex9.py"""
+    """Run examples/ex09.py"""
     def runTest(self):
-        import examples.ex9
-        self.assertAlmostEqual(np.max(examples.ex9.x), 0.054313059564406921)
+        import examples.ex09
+        self.assertAlmostEqual(np.max(examples.ex09.x), 0.054313059564406921)
+
