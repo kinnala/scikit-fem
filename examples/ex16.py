edd21153c5552941c03239809b9f91a696b94e93
Tom Gustafsson <jkinnala@gmail.com>
2018-08-07 22:05:47 +0300

    Renamed Legendre example. Added test that runs the example.

--- examples/ex16.py
+++ examples/ex16.py
@@ -7,7 +7,7 @@ Legendre's equation in self-adjoint Sturm–Liouville form is
 
   {(1 - x**2) u'}' + k u = 0      (-1 < x < 1)
 
-The eigenvalues are k = n * (n + 1) for n = 0, 1, 2, …  The
+The eigenvalues are k = n * (n + 1) for n = 0, 1, 2, ...  The
 conventional normalization is u(1) = 1 and
 
    <u, u> = 2 / (2 * n + 1).
@@ -17,7 +17,7 @@ accessed inside the bilinear_form as w[0][0].
 
 """
 
-from matplotlib.pyplot import subplots
+from matplotlib.pyplot import subplots, show
 import numpy as np
 from scipy.sparse.linalg import eigsh
 from scipy.special import legendre
@@ -30,22 +30,21 @@ m = MeshLine(x)
 e = ElementLineP1()
 basis = InteriorBasis(m, e)
 
-
 @bilinear_form
 def stiffness(u, du, v, dv, w):
-    return dv[0] * (1 - w[0][0]**2) * du[0]
-
+    return (1 - w[0][0]**2) * du[0] * dv[0]
 
 L = asm(stiffness, basis)
 M = asm(mass, basis)
 
-k, u = eigsh(L, M=M, sigma=0.)
-u = u * np.sqrt([2 / (2 * n + 1) for n in range(len(k))]) * np.sign(u[-1, :])
+ks, u = eigsh(L, M=M, sigma=0.)
+u = u * np.sqrt([2 / (2 * n + 1) for n in range(len(ks))]) * np.sign(u[-1, :])
 
 fig, ax = subplots()
-for n, (k, u) in enumerate(zip(k, u.T)):
+for n, (k, u) in enumerate(zip(ks, u.T)):
     dots, = ax.plot(x, u, label=n, marker='o', linestyle='None')
     ax.plot(x, legendre(n)(x), color=dots.get_color())
-    print('{:2d}  {:5.2f}'.format(n * (n + 1), k))
-fig.legend()
-fig.savefig('legendre')
+
+if __name__ == "__main__":
+    ax.legend()
+    show()
