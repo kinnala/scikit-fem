25f4fafe2d6592ef8ac0b119ed60032398775cf6
G. D. McBain <gdmcbain@protonmail.com>
2018-08-07 16:52:59 +1000

    simplified use of sum in bilinear_form laplace

--- skfem/models/poisson.py
+++ skfem/models/poisson.py
@@ -6,7 +6,7 @@ from skfem.assembly import bilinear_form, linear_form
 
 @bilinear_form
 def laplace(u, du, v, dv, w):
-    return sum(du[k]*dv[k] for k in range(du.shape[0]))
+    return sum(du*dv)
 
 @bilinear_form
 def mass(u, du, v, dv, w):
