c6d1f0d9c126212281541a11e999b847f4da92c3
Tom Gustafsson <kinnala@users.noreply.github.com>
2018-11-17 12:40:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='scikit-fem',
-    version='0.1.17',
+    version='0.1.18',
     description='Simple finite element assemblers',
     long_description='Easy to use finite element assemblers and related tools. See Github page for more information and examples.',  # Optional
     url='https://github.com/kinnala/scikit-fem',
