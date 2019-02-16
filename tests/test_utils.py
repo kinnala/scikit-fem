fbeac50efb45cff0cea18500c1108604d528e138
Tom Gustafsson <enigustom@eniram.fi>
2019-02-16 20:31:33 +0200

    Forgot to add test for L2_projection.

---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*- 
+"""Unit tests for utils package."""
+
+import unittest
+import numpy as np
+
+from skfem.assembly import asm, InteriorBasis
+from skfem.element import ElementTriP1
+from skfem.utils import L2_projection
+from skfem.mesh import MeshTri
+
+class InitializeScalarField(unittest.TestCase):
+    def runTest(self):
+        mesh = MeshTri()
+        mesh.refine(5)
+        basis = InteriorBasis(mesh, ElementTriP1())
+        
+        def fun(x, y):
+            return x**2 + y**2
+        
+        x = L2_projection(fun, basis)
+        y = fun(*mesh.p)
+
+        normest = np.linalg.norm(x - y)
+
+        self.assertTrue(normest < 0.011,
+                        msg = "|x-y| = {}".format(normest))
