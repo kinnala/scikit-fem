d99a2002e4b5565c0feffcf81517a5a5f5674e97
G. D. McBain <gdmcbain@protonmail.com>
2019-03-05 09:38:29 +1100

    FacetBasis in L2_projection for coordinate-dependent Dirichlet data #112 #132

--- docs/examples/ex14.py
+++ docs/examples/ex14.py
@@ -1,6 +1,8 @@
 from skfem import *
 from skfem.models.poisson import laplace
 
+import numpy as np
+
 m = MeshTri()
 m.refine(4)
 
@@ -12,13 +14,17 @@ A = asm(laplace, basis)
 D = basis.get_dofs().all()
 I = basis.complement_dofs(D)
 
+
 def dirichlet(x, y):
     """return a harmonic function"""
     return ((x + 1.j * y) ** 2).real
 
 
-u = L2_projection(dirichlet, basis)
-u[I] = solve(*condense(A, 0.*u, u, I))
+u = np.zeros(basis.N)
+u[D] = L2_projection(dirichlet,
+                     FacetBasis(m, e, facets=m.boundary_facets()), D)
+u[I] = solve(*condense(A, np.zeros_like(u), u, I))
+
 
 if __name__ == "__main__":
     print('||grad u||**2 = {:f} (exact = 8/3 = {:f})'.format(u @ A @ u, 8/3))
