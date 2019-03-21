correct sign on nonlinear terms!

docs/examples/ns.py
@@ -182,7 +182,7 @@ class BackwardFacingStep:
 
     def f(self, uvp: np.ndarray, reynolds: float) -> np.ndarray:
         """Return the residual of a given velocity-pressure solution"""
-        out = self.S @ uvp + reynolds * self.N(uvp)
+        out = self.S @ uvp - reynolds * self.N(uvp)
 
         out[self.D] = uvp[self.D] - self.make_vector()[self.D]
         return out
@@ -199,7 +199,7 @@ class BackwardFacingStep:
         duvp = np.zeros_like(uvp)
         u = self.basis['u'].interpolate(self.split(uvp)[0])
         duvp[self.I] = solve(*condense(
-            self.S +
+            self.S -
             reynolds
             * block_diag([asm(acceleration_jacobian, self.basis['u'], w=u),
                           csr_matrix((self.basis['p'].N,)*2)]),
@@ -207,7 +207,7 @@ class BackwardFacingStep:
         return duvp
 
 
-bfs = BackwardFacingStep(lcar=.5**4)
+bfs = BackwardFacingStep(lcar=.5**3)
 
 
 class RangeException(Exception):