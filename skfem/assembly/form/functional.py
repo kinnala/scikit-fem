40f9b3a2ad3b920ba8271e2c9488ffe4f003d4b0
G. D. McBain <gdmcbain@protonmail.com>
2019-06-21 09:38:15 +1000

    dropped un- or ab- used ubasis argument from Functional methods

--- skfem/assembly/form/functional.py
+++ skfem/assembly/form/functional.py
@@ -16,18 +16,15 @@ class Functional(Form):
         return np.sum(self.form(w) * dx, axis=1)
 
     def elemental(self,
-                 ubasis: GlobalBasis,
-                 vbasis: Optional[GlobalBasis] = None,
-                 w: Optional[Any] = (None, None, None)) -> ndarray:
-        assert vbasis is None
-        vbasis = ubasis
+                  vbasis: GlobalBasis,
+                  w: Optional[Any] = (None, None, None)) -> ndarray:
         return self.kernel(self.parameters(w, vbasis), vbasis.dx)
 
     def assemble(self,
-                 ubasis: GlobalBasis,
-                 vbasis: Optional[GlobalBasis] = None,
+                 vbasis: GlobalBasis,
                  w: Optional[Any] = (None, None, None)) -> ndarray:
-        return sum(self.elemental(ubasis, vbasis, w))
+        return sum(self.elemental(vbasis, w))
+
 
 def functional(form: Callable) -> Functional:
     return Functional(form)
