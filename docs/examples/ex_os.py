02d0b49218596597e1de42a9cdca28b41ba8c349
G. D. McBain <gdmcbain@protonmail.com>
2019-11-01 14:02:42 +1100

    new Orr–Sommerfeld example

---
+++ docs/examples/ex_os.py
@@ -0,0 +1,77 @@
+from skfem import *
+from skfem.models.poisson import laplace, mass
+
+from pathlib import Path
+
+from matplotlib.pyplot import subplots
+import numpy as np
+from scipy.sparse import block_diag, bmat, csr_matrix
+from scipy.sparse.linalg import eigs
+
+
+@bilinear_form
+def divergence(u, du, v, dv, w):
+    """Can't use models.general.divergence in one dimension"""
+    return v * du[0]
+
+
+@bilinear_form
+def base_velocity(u, du, v, dv, w):
+    """plane Poiseuille flow, normalized by centre-line velocity"""
+    return v * (1 - w.x[0]**2) * u
+
+
+@bilinear_form
+def base_shear(u, du, v, dv, w):
+    """plane Poiseuille flow, normalized by centre-line velocity"""
+    return -2 * v * w.x[0] * u
+
+
+mesh = MeshLine(np.linspace(0, 1, 2**6))
+element = {'u': ElementLineP2(), 'p': ElementLineP1()}
+basis = {v: InteriorBasis(mesh, e, intorder=4) for v, e in element.items()}
+
+L = asm(laplace, basis['u'])
+M = asm(mass, basis['u'])
+P = asm(mass, basis['u'], basis['p'])
+B = -asm(divergence, basis['u'], basis['p'])
+V = asm(base_velocity, basis['u'])
+W = asm(base_shear, basis['u'])
+
+re = 1e4                        # Reynolds number
+alpha = 1.                      # longitudinal wavenumber
+jare = 1j * alpha * re
+Kuu = jare * V + alpha**2 * M + L
+stiffness = bmat([[Kuu,       re * W, jare * P.T],
+                  [None,      Kuu,    re * B.T],
+                  [-jare * P, re * B, None]], 'csc')
+mass_matrix = block_diag([M, M, csr_matrix((basis['p'].N,)*2)], 'csr')
+
+# Seek only 'even' modes, 'even' in terms of conventional
+# stream-function formulation, so that the longitudinal component u of
+# the perturbation to the velocity vanishes on the centre-line z = 0,
+# z here being the sole coordinate.
+
+u_boundaries = basis['u'].get_dofs().all()
+walls = np.concatenate([u_boundaries,
+                        u_boundaries[1:] + basis['u'].N])
+
+pencil = condense(stiffness, mass_matrix, D=walls, expand=False)
+c = {'skfem': eigs(pencil[0], M=pencil[1], k=2**5, sigma=0.,
+                   return_eigenvectors=False) / jare,
+     'exact': np.loadtxt(Path(__file__).with_suffix('.csv'), dtype=complex)}
+
+
+if __name__ == '__main__':
+    fig, ax = subplots()
+    for (label, wavespeed), marker in zip(c.items(), 'o+'):
+        ax.plot(wavespeed.real, wavespeed.imag,
+                marker=marker, linestyle='None', label=label)
+    fig.suptitle('even modes of plane Poiseuille flow (Re=10⁴, α=1)')
+    ax.set_xlabel(r'real wavespeed, $\Re c$')
+    ax.set_xlim((0, 1))
+    ax.set_ylabel(r'imaginary wavespeed, $\Im c$')
+    ax.set_ylim((-.8, max(c['skfem'].imag)))
+    ax.grid(True)
+    ax.legend(loc=3)
+    fig.savefig(Path(__file__).with_suffix('.png'))
---
+++ docs/examples/ex_os.rst
@@ -0,0 +1,29 @@
+Linear hydrodynamic stability
+-----------------------------
+
+The linear stability of one-dimensional solutions of the Navier–Stokes equations is governed by the `Orr–Sommerfeld equation <https://en.wikipedia.org/wiki/Orr%E2%80%93Sommerfeld_equation>`_ (Drazin & Reid 2004, p. 156).  This is expressed in terms of the stream-function of the perturbation, giving a two-point boundary value problem for a complex fourth-order ordinary differential equation.
+
+The fourth-order derivatives would require :math:`C^1` finite elements, e.g. of the Hermite family (Mamou & Khalid 2004); however, an equivalent problem is given by a second-order system for the pressure and the longitudinal and transverse components of velocity (Drazin & Reid 2004, eq. 25.9, p. 155):
+
+.. math::
+   
+   \left(\mathrm j\alpha R U(z) + \alpha^2 - \frac{\mathrm d^2}{\mathrm dz^2}\right)u + RU'(z)w + \mathrm j\alpha  Rp &= \mathrm j\alpha Rc u \\
+   \left(\mathrm j\alpha R U(z) + \alpha^2 - \frac{\mathrm d^2}{\mathrm dz^2}\right)w + R\frac{\mathrm dp}{\mathrm dz} &= \mathrm j\alpha Rc w \\
+   \mathrm j\alpha u + \frac{\mathrm dw}{\mathrm dz} &= 0
+   
+where :math:`U(z)` is the base velocity profile, :math:`\alpha` is the longitudinal wavenumber of the perturbation, and :math:`R` is the Reynolds number.  This system can be discretized using one-dimensional Taylor–Hood elements, analogous to the two-dimensional elements used for the :ref:`stokesex` and :ref:`navierstokes`.
+
+The classical test-case for this problem is plane Poiseuille flow :math:`U(z) = 1 - z^2` on :math:`-1 < z < 1` at :math:`\alpha = 1` and :math:`R = 10^4` (Drazin & Reid 2004, figure 4.19; Mamou & Khalid 2004), typically seeking only the 'even' modes for which :math:`u(0) = u(1) = w'(0) = w(1) = 0`.
+
+
+.. figure:: ex_os.png
+
+    The spectrum of 'even'-mode complex wavespeeds for plane Poiseuille flow at Re = 10 000 and :math:`\alpha = 1` (cf. Criminal, Jackson, & Joslin 2003, table 3.1; Drazin & Reid 2004, figure 4.19)
+
+
+.. literalinclude:: ex_os.py
+   :linenos:
+
+* Criminale, W. O., Jackson, T. L.,, Joslin, R. D. (2003). *Theory and Computation in Hydrodynamic Stability.* Cambridge: Cambridge University Press.
+*  Drazin, P. G., Reid, W. H. (2004). *Hydrodynamic Stability.* Cambridge University Press
+*  Mamou, M. & Khalid, M. (2004). Finite element solution of the Orr–Sommerfeld equation using high precision Hermite elements: plane Poiseuille flow. *International Journal for Numerical Methods in Fluids* 44. pp. 721–735. `doi:10.1002/fld.661 <https://doi.org/10.1002/fld.661>`_
