3be7898d608bf8298c3136db236b654fd2dd58b8
G. D. McBain <gdmcbain@protonmail.com>
2019-11-04 12:29:51 +1100

    test Orr-Sommerfeld spectrum against published reference data

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -207,5 +207,15 @@ class TestEx28(unittest.TestCase):
         self.assertAlmostEqual(*t.values(), delta=2e-4)
 
 
+class TestExOS(unittest.TestCase):
+    def runTest(self):
+        from docs.examples.ex_os import c
+        wavespeed = tuple(
+            np.array(sorted(wavespeed, key=np.imag, reverse=True))
+            for label, wavespeed in c.items())
+        self.assertLess(np.linalg.norm(wavespeed[1] - wavespeed[0], np.inf),
+                        5e-3)
+
+
 if __name__ == '__main__':
     unittest.main()
