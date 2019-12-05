89f554a7a55ddbb62fa3212535c1f9e3d0eff72f
G. D. McBain <gdmcbain@protonmail.com>
2019-12-05 15:54:09 +1100

    trying pyamg.smoothed_aggregation_solver #264

--- docs/examples/ex09.py
+++ docs/examples/ex09.py
@@ -27,6 +27,33 @@ x[I] = solve(Aint, bint, solver=solver_iter_pcg(verbose=verbose))
 x[I] = solve(Aint, bint,
              solver=solver_iter_pcg(M=build_pc_ilu(Aint), verbose=verbose))
 
+try:
+    from pyamg import smoothed_aggregation_solver
+
+    tol = 1e-6
+    def solver_amg_sa(**kwargs) -> 'LinearSolver':
+
+        if kwargs.pop('verbose', False):
+
+            def callback(x):
+                print(np.linalg.norm(x))
+
+            kwargs['callback'] = callback
+                    
+        def solver(A, b):
+            return smoothed_aggregation_solver(A).solve(b, **kwargs)
+
+        return solver
+
+    x[I] = solve(Aint, bint,
+                 solver=solver_amg_sa(verbose=verbose,
+                                      tol=tol,
+                                      accel='cg'))
+    print(smoothed_aggregation_solver.__name__, 'converged to tol =', tol)
+    
+except ImportError:
+    print('Skipping PyAMG')
+    
 if verbose:
     from os.path import splitext
     from sys import argv
