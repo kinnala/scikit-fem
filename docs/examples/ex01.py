17781b971da783c8f7446f23b62616cccf719f49
Tom Gustafsson <tom.gustafsson@iki.fi>
2019-12-30 19:16:42 +0200

    Fix examples 1 and 2

--- docs/examples/ex01.py
+++ docs/examples/ex01.py
@@ -22,7 +22,7 @@ x = solve(*condense(A, b, I=m.interior_nodes()))
 if __name__ == "__main__":
     from os.path import splitext
     from sys import argv
-    from skfem.visualize import plot, savefig
+    from skfem.visuals.matplotlib import plot, savefig
     
     plot(m, x, smooth=True, colorbar=True)
     savefig(splitext(argv[0])[0] + '_solution.png')
