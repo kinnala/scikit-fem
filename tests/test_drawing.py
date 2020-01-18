30e1b92565b0493d4e06c3692563ff79b3095e2a
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-01-18 20:56:22 +0200

    Add more tests that should fail

--- tests/test_drawing.py
+++ tests/test_drawing.py
@@ -9,7 +9,6 @@ class CallDraw(unittest.TestCase):
     mesh_type = MeshTri
 
     def runTest(self):
-        # Mesh.remove_elements
         m = self.mesh_type()
         draw(m)
 
@@ -20,3 +19,27 @@ class CallDrawQuad(CallDraw):
 
 class CallDrawTet(CallDraw):
     mesh_type = MeshTet
+
+
+class CallPlot(unittest.TestCase):
+    mesh_type = MeshTri
+
+    def runTest(self):
+        m = self.mesh_type()
+        plot(m, m.p[0])
+
+
+class CallPlotQuad(CallPlot):
+    mesh_type = MeshQuad
+
+
+class CallPlotLine(CallPlot):
+    mesh_type = MeshLine
+
+
+class CallPlot3(unittest.TestCase):
+    mesh_type = MeshTri
+
+    def runTest(self):
+        m = self.mesh_type()
+        plot3(m, m.p[0])
