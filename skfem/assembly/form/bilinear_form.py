76eeb8c01e2aef6d2fb62f15cb1de30af867b601
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-01-21 15:24:06 +0200

    Start experimenting with new form API

--- skfem/assembly/form/bilinear_form.py
+++ skfem/assembly/form/bilinear_form.py
@@ -11,18 +11,6 @@ from ..basis import Basis
 
 class BilinearForm(Form):
 
-    def kernel(self,
-               A: ndarray,
-               ix: ndarray,
-               ubasis: BasisTuple,
-               vbasis: BasisTuple,
-               w: FormParameters,
-               dx: ndarray) -> None:
-        for k in range(ix.shape[0]):
-            i, j = ix[k]
-            A[i, j] = np.sum(self.form(*ubasis[j], *vbasis[i], w) * dx,
-                             axis=1)
-
     def assemble(self,
                  ubasis: Basis,
                  vbasis: Optional[Basis] = None,
@@ -63,28 +51,13 @@ class BilinearForm(Form):
         # create sparse matrix indexing
         for j in range(ubasis.Nbfun):
             for i in range(vbasis.Nbfun):
-                # find correct location in data,rows,cols
+                # find correct location in data, rows, cols
                 ixs = slice(nt * (vbasis.Nbfun * j + i),
                             nt * (vbasis.Nbfun * j + i + 1))
-                rows[ixs] = vbasis.element_dofs[i, :]
-                cols[ixs] = ubasis.element_dofs[j, :]
-
-        # create indices for linear loop over local stiffness matrix
-        ixs = [i for j, i in product(range(ubasis.Nbfun), range(vbasis.Nbfun))]
-        jxs = [j for j, i in product(range(ubasis.Nbfun), range(vbasis.Nbfun))]
-        indices = np.array([ixs, jxs]).T
-
-        # split local stiffness matrix elements to threads
-        threads = [threading.Thread(
-            target=self.kernel,
-            args=(data, ij, ubasis.basis, vbasis.basis, w, dx))
-                   for ij in np.array_split(indices, nthreads, axis=0)]
-
-        # start threads and wait for finishing
-        for t in threads:
-            t.start()
-        for t in threads:
-            t.join()
+                rows[ixs] = vbasis.element_dofs[i]
+                cols[ixs] = ubasis.element_dofs[j]
+                data[ixs] = np.sum(self.form(*ubasis[j], *vbasis[i], w) * dx,
+                                   axis=1)
 
         K = coo_matrix((np.transpose(data, (1, 0, 2)).flatten('C'),
                         (rows, cols)),
