578ca4601df2f8e83672ea3e5c3fb3446e5c5782
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-02-01 15:18:45 +0200

    Add convergence test for Raviart-Thomas element

--- skfem/element/element_tri/element_tri_rt0.py
+++ skfem/element/element_tri/element_tri_rt0.py
@@ -7,20 +7,23 @@ class ElementTriRT0(ElementHdiv):
     dim = 2
     maxdeg = 1
     dofnames = ['u^n']
+    doflocs = np.array([[.5, 0.],
+                        [.5, .5],
+                        [0., .5]])
 
     def lbasis(self, X, i):
-        x, y = X[0, :], X[1, :]
+        x, y = X
 
         if i == 0:
             phi = np.array([x, y - 1])
-            dphi = 2 + 0*x
+            dphi = 2 + 0 * x
         elif i == 1:
             phi = np.array([x, y])
-            dphi = 2 + 0*x
+            dphi = 2 + 0 * x
         elif i == 2:
             phi = np.array([x - 1, y])
-            dphi = 2 + 0*x
+            dphi = 2 + 0 * x
         else:
-            raise Exception("!")
+            self._index_error()
 
         return phi, dphi
