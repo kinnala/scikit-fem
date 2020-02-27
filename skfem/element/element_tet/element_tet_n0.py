ce9101472232fd54208fc9e73d243f7bd413635f
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-02-27 13:10:59 +0200

    Add doflocs to ElementTetN0

--- skfem/element/element_tet/element_tet_n0.py
+++ skfem/element/element_tet/element_tet_n0.py
@@ -8,29 +8,47 @@ class ElementTetN0(ElementHcurl):
     dim = 3
     maxdeg = 1
     dofnames = ['u^t']
+    doflocs = np.array([[.5, 0., 0.],
+                        [.5, .5, 0.],
+                        [0., .5, 0.],
+                        [0., 0., .5],
+                        [.5, 0., .5],
+                        [0., .5, .5],])
     mesh_type = MeshTet
 
     def lbasis(self, X, i):
-        x, y, z = X[0, :], X[1, :], X[2, :]
+        x, y, z = X
 
         if i == 0:
-            phi = np.array([1-z-y, x, x])
-            dphi = np.array([0*x, -2 + 0*x, 2 + 0*x])
+            phi = np.array([1 - z - y, x, x])
+            dphi = np.array([0 * x,
+                             -2 + 0 * x,
+                             2 + 0 * x])
         elif i == 1:
-            phi = np.array([-y, x, 0*z])
-            dphi = np.array([0*x, 0*x, 2 + 0*x])
+            phi = np.array([-y, x, 0 * z])
+            dphi = np.array([0 * x,
+                             0 * x,
+                             2 + 0 * x])
         elif i == 2:
-            phi = np.array([y, 1-z-x, y])
-            dphi = np.array([2 + 0*x, 0*x, -2 + 0*x])
+            phi = np.array([y, 1 - z - x, y])
+            dphi = np.array([2 + 0 * x,
+                             0 * x,
+                             -2 + 0 * x])
         elif i == 3:
-            phi = np.array([z, z, 1-x-y])
-            dphi = np.array([-2 + 0*x, 2 + 0*x, 0*x])
+            phi = np.array([z, z, 1 - x - y])
+            dphi = np.array([-2 + 0 * x,
+                             2 + 0 * x,
+                             0 * x])
         elif i == 4:
-            phi = np.array([-z, 0*y, x])
-            dphi = np.array([0*x, -2 + 0*x, 0*x])
+            phi = np.array([-z, 0 * y, x])
+            dphi = np.array([0 * x,
+                             -2 + 0 * x,
+                             0 * x])
         elif i == 5:
-            phi = np.array([0*x, -z, y])
-            dphi = np.array([2 + 0*x, 0*x, 0*x])
+            phi = np.array([0 * x, -z, y])
+            dphi = np.array([2 + 0 * x,
+                             0 * x,
+                             0 * x])
         else:
             self._index_error()
 
