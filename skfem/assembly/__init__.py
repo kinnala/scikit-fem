baed3bccf5396408b0a1701c5822d9de7ea73f51
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-04-23 21:54:47 +0300

    Start improving the documentation

--- skfem/assembly/__init__.py
+++ skfem/assembly/__init__.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-"""This module contains classes and functions related to the
-construction of finite element matrices.
+"""The basic workflow for finite element assembly is the following:
 
-The basic workflow is the following:
-
-1. Create :class:`~skfem.assembly.InteriorBasis` and/or :class:`~skfem.assembly.FacetBasis`
-   objects.
-2. Define the bilinear and linear forms using the decorators :func:`~skfem.assembly.bilinear_form`
-   and :func:`~skfem.assembly.linear_form`.
-3. Assemble finite element matrices using :func:`~skfem.assembly.asm`.
+1. Initialize :class:`~skfem.mesh.Mesh` and :class:`~skfem.mesh.Element`.
+2. Create :class:`~skfem.assembly.InteriorBasis` and/or
+   :class:`~skfem.assembly.FacetBasis` objects.
+3. Define the forms using the decorators :class:`~skfem.assembly.BilinearForm`,
+   :class:`~skfem.assembly.LinearForm`, and :class:`~skfem.assembly.Functional`.
+4. Assemble using :func:`~skfem.assembly.asm`.
 
 """
 
--- skfem/mesh/__init__.py
+++ skfem/mesh/__init__.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
-"""This module contains different types of finite element meshes.
-
-Meshes are created using various built-in constructors or loaded from external
-formats using `meshio <https://github.com/nschloe/meshio>`_. See the following
-implementations:
+"""This module defines different types of finite element meshes.  Meshes can be
+created using various built-in constructors or loaded from external formats
+using `meshio <https://github.com/nschloe/meshio>`_.  The supported types are
 
 - :class:`~skfem.mesh.MeshTri`, triangular mesh
 - :class:`~skfem.mesh.MeshQuad`, quadrilateral mesh
 - :class:`~skfem.mesh.MeshTet`, tetrahedral mesh
 - :class:`~skfem.mesh.MeshHex`, hexahedral mesh
 - :class:`~skfem.mesh.MeshLine`, one-dimensional mesh
+
+For example, initializing the default two triangle mesh for the unit square can
+be done as follows:
+
+>>> from skfem.mesh import MeshTri
+>>> MeshTri()
+Triangular mesh with 4 vertices and 2 elements.
 
 """
 
