23296b0deb59c6d431caaaf7205a84e6e81210e8
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-05-18 12:23:21 +0300

    Make facet expansion test more complicated

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -145,8 +145,10 @@ class TestBoundaryEdges(unittest.TestCase):
 
     def runTest(self):
         m = MeshTet()
+        # default mesh has all edges on the boundary
+        self.assertTrue(len(m.boundary_edges()) == m.edges.shape[1])
         m.refine()
-        # check that there is a correct amount of edges:
+        # check that there is a correct amount of boundary edges:
         # 12 (cube edges) * 2 (per cube edge) + 6 (cube faces) * 8 (per cube face)
         # = 72 edges
         self.assertTrue(len(m.boundary_edges()) == 72)
