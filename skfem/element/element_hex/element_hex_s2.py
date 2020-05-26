25ffdb68a822d0339a7b7079d47655167c34ac73
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-05-26 22:50:30 +0300

    Fix the syntax error

--- skfem/element/element_hex/element_hex_s2.py
+++ skfem/element/element_hex/element_hex_s2.py
@@ -78,8 +78,8 @@ class ElementHexS2(ElementH1):
             dx = 2.0 * s[0] if isinstance(s[0], np.ndarray) else s[0]
             dy = 2.0 * s[1] if isinstance(s[1], np.ndarray) else s[1]
             dz = 2.0 * s[2] if isinstance(s[2], np.ndarray) else s[2]
-            dphi = np.array([dx * (1 + y) * (1 + z)
-                             dy * (1 + x) * (1 + z)
+            dphi = np.array([dx * (1 + y) * (1 + z),
+                             dy * (1 + x) * (1 + z),
                              dz * (1 + x) * (1 + y)]) / 4
         else:
             self._index_error()
