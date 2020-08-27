95114d583464429b287c13da5705c36e64418431
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-08-27 22:25:44 +0300

    PEP8 knee-jerk

--- docs/examples/ex36.py
+++ docs/examples/ex36.py
@@ -1,4 +1,5 @@
 r"""Incompressible Hyperelasticity
+
 This example solves the governing equations describing the mechanical 
 response of a nearly incompressible elastomer using a mixed formulation. 
 The elastomer, assumed to be made up of a Neo-Hookean solid, occupies the domain 
@@ -7,15 +8,17 @@ given by
 .. math::
    \int_\Omega\Psi(\mathbf{F})d\mathbf{X};\qquad\Psi(\mathbf{F}) = \mu/2 (I_1 - 3) - \mu \ln(J) + \lambda/2(J-1)^2
 
-where :math:`I_1 = \mathbf{F}:\mathbf{F} = tr(\mathbf{F}^T\mathbf{F})` and 
-:math:`J = \text{det}(\mathbf{F})` and :math:`\mathbf{F} = \mathbf{I} + \nabla\mathbf{u}` is the deformation gradient tensor. 
-The standard variational formulation in the absence of body force and surface traction can be written as
+where :math:`I_1 = \mathbf{F}:\mathbf{F} = tr(\mathbf{F}^T\mathbf{F})` and
+:math:`J = \text{det}(\mathbf{F})` and :math:`\mathbf{F} = \mathbf{I} +
+\nabla\mathbf{u}` is the deformation gradient tensor.  The standard variational
+formulation in the absence of body force and surface traction can be written as
 .. math::
    \min_{u\in\mathcal{K}} \int_\Omega \Psi(\mathbf{F})d\mathbf{X}
 
-where :math:`\mathcal{K}` is a set of kinematically admissible fields that satisfy the Dirichlet boundary condition(s). 
-However, this becomes ill-posed when :math:`\lambda/\mu\rightarrow +\infty`. In order to 
-circumvent this issue, we consider a mixed variational formulation, namely
+where :math:`\mathcal{K}` is a set of kinematically admissible fields that
+satisfy the Dirichlet boundary condition(s).  However, this becomes ill-posed
+when :math:`\lambda/\mu\rightarrow +\infty`. In order to circumvent this issue,
+we consider a mixed variational formulation, namely
 
 .. math::
    \min_{\mathbf{u}\in\mathcal{K}}\max_{p}\int_\Omega \Psi*(\mathbf{F}, p)d\mathbf{X}
@@ -25,9 +28,10 @@ where
 .. math::
    \Psi*(\mathbf{F}, p) = p (J-J*) + mu/2(I_1-3) - \mu\ln(J*) + \lambda/2(J*-1)^2
 
-and :math:`J* = (\lambda + p + \sqrt{(\lambda + p)^2 + 4\lambda\mu )}/(2\lambda)`. 
-The numerical solution to the above problem requires choosing stable finite element spaces for the displacement (:math:`\mathbf{u}`)
-and pressure (:math:`p`). The corresponding weak form is given by
+and :math:`J* = (\lambda + p + \sqrt{(\lambda + p)^2 + 4\lambda\mu)}/(2\lambda)`.
+The numerical solution to the above problem requires choosing stable finite
+element spaces for the displacement (:math:`\mathbf{u}`) and pressure
+(:math:`p`). The corresponding weak form is given by
 
 find :math:`(\mathbf{u},p)\in (V_1 x V_2)` such that 
 
@@ -49,46 +53,61 @@ and
 .. math::
    V_2 = \left\{ p\ni p\in L^2(\Omega) \right\}
 
-Here, inspired by it's counterpart in fluid mechanics, we choose the lowest order Taylor-Hood element (:math:`\mathbb{P}_2-P_1`) 
-which satifies the Babuska-Brezzi condition. Fore more details on the derivation, see http://pamies.cee.illinois.edu/Publications_files/IJNME_2015.pdf#page=4&zoom=100,312,414
-The weak forms above result in a system of nonlinear algebraic equations for the degrees of freedom , and therefore needs to be solved using 
-a nonlinear solver. In the example below, we linearize :math:`\mathcal{F}_1` and :math:`\mathcal{F}_2` and setup solve for incremental displacement 
-and pressure dofs.
+Here, inspired by it's counterpart in fluid mechanics, we choose the lowest
+order Taylor-Hood element (:math:`\mathbb{P}_2-P_1`) which satifies the
+Babuska-Brezzi condition. Fore more details on the derivation, see
+http://pamies.cee.illinois.edu/Publications_files/IJNME_2015.pdf#page=4&zoom=100,312,414
+The weak forms above result in a system of nonlinear algebraic equations for
+the degrees of freedom , and therefore needs to be solved using a nonlinear
+solver. In the example below, we linearize :math:`\mathcal{F}_1` and
+:math:`\mathcal{F}_2` and setup solve for incremental displacement and pressure
+dofs.
 
-The following demonstrates uniaxial tension in one direction, and the lateral edges allowed to remain free. The geometry is a homogeneous unit cube 
-made up of a Neo-Hookean solid with :math:`\lambda/\mu = 1000`. For this loading and geometry, in the limit of :math:`\lambda/\mu\rightarrow +\infty`, 
-the deformation gradient would be given by :math:`\mathbf{F} = \text{diag}(\lambda,1/\sqrt{\lambda})` and the pressure field admits a closed form solution 
-:math:`p=-\mu/\ell` where :math:`\ell` is the applied stretch. 
+The following demonstrates uniaxial tension in one direction, and the lateral
+edges allowed to remain free. The geometry is a homogeneous unit cube made up
+of a Neo-Hookean solid with :math:`\lambda/\mu = 1000`. For this loading and
+geometry, in the limit of :math:`\lambda/\mu\rightarrow +\infty`, the
+deformation gradient would be given by :math:`\mathbf{F} =
+\text{diag}(\lambda,1/\sqrt{\lambda})` and the pressure field admits a closed
+form solution :math:`p=-\mu/\ell` where :math:`\ell` is the applied stretch.
+
 """
-from numpy import einsum, linalg as nla, zeros, zeros_like, concatenate, split as npsplit, hstack, abs as npabs, arange, sqrt
+from numpy import (einsum, linalg as nla, zeros,
+                   zeros_like, concatenate, split as npsplit,
+                   hstack, abs as npabs, arange, sqrt)
 from scipy.sparse import bmat
 from skfem.helpers import grad, transpose, det, inv
 from skfem import *
 
+
 mu, lmbda = 1., 1.e3
+
 
 def F1(w):
     u = w["disp"]
     p = w["press"]
     F = zeros_like(grad(u))
     for i in range(3):
-        F[i,i] += 1.
+        F[i, i] += 1.
     F += grad(u)
     J = det(F)
     Finv = inv(F)
     return p * J * transpose(Finv) + mu * F
+
 
 def F2(w):
     u = w["disp"]
     p = w["press"].value
     F = zeros_like(grad(u))
     for i in range(3):
-        F[i,i] += 1.
+        F[i, i] += 1.
     F += grad(u)
     J = det(F)
-    Js = 0.5*(lmbda + p + 2.0*sqrt(lmbda*mu + 0.25*(lmbda + p)**2))/lmbda
-    dJsdp=(0.25*lmbda + 0.25*p + 0.5*sqrt(lmbda*mu + 0.25*(lmbda + p)**2))/(lmbda*sqrt(lmbda*mu + 0.25*(lmbda + p)**2))
-    return J - (Js + (p + mu/Js - lmbda*(Js - 1)) * dJsdp)
+    Js = .5 * (lmbda + p + 2. * sqrt(lmbda * mu + .25 * (lmbda + p) ** 2)) / lmbda
+    dJsdp = ((.25 * lmbda + .25 * p + .5 * sqrt(lmbda * mu + .25 * (lmbda + p) ** 2))
+             / (lmbda * sqrt(lmbda * mu + .25 * (lmbda + p) ** 2)))
+    return J - (Js + (p + mu / Js - lmbda * (Js - 1)) * dJsdp)
+
 
 def A11(w):
     u = w["disp"]
@@ -96,13 +115,16 @@ def A11(w):
     F = zeros_like(grad(u))
     eye = zeros_like(grad(u))
     for i in range(3):
-        F[i,i] += 1.
-        eye[i,i] += 1.
+        F[i, i] += 1.
+        eye[i, i] += 1.
     F += grad(u)
     J = det(F)
     Finv= inv(F)
-    L = p * J * einsum("lk...,ji...->ijkl...", Finv, Finv) - p * J * einsum("jk...,li...->ijkl...", Finv, Finv) + mu * einsum("ik...,jl...->ijkl...", eye, eye)
+    L = (p * J * einsum("lk...,ji...->ijkl...", Finv, Finv)
+         - p * J * einsum("jk...,li...->ijkl...", Finv, Finv)
+         + mu * einsum("ik...,jl...->ijkl...", eye, eye))
     return L
+
 
 def A12(w):
     u = w["disp"]
@@ -114,16 +136,18 @@ def A12(w):
     Finv = inv(F)
     return J * transpose(Finv)
 
+
 def A22(w):
     u = w["disp"]
     p = w["press"].value
-    
-    Js = 0.5*(lmbda + p + 2.0*sqrt(lmbda*mu + 0.25*(lmbda + p)**2))/lmbda
-    dJsdp=(0.25*lmbda + 0.25*p + 0.5*sqrt(lmbda*mu + 0.25*(lmbda + p)**2))/(lmbda*sqrt(lmbda*mu + 0.25*(lmbda + p)**2))
-    d2Jdp2 = 0.25*mu/(lmbda*mu + 0.25*(lmbda + p)**2)**(3/2)
-    L = -2. * dJsdp - p * d2Jdp2 + mu/Js**2 * dJsdp**2 - mu/Js * d2Jdp2 + lmbda * (Js - 1.) * d2Jdp2 + lmbda * dJsdp**2
+    Js = .5 * (lmbda + p + 2. * sqrt(lmbda * mu + .25 * (lmbda + p) ** 2)) / lmbda
+    dJsdp = ((.25 * lmbda + .25 * p + .5 * sqrt(lmbda * mu + .25 * (lmbda + p) ** 2))
+             / (lmbda * sqrt(lmbda * mu + .25 * (lmbda + p) ** 2)))
+    d2Jdp2 = .25 * mu / (lmbda * mu + .25 * (lmbda + p) ** 2) ** (3/2)
+    L = (-2. * dJsdp - p * d2Jdp2 + mu / Js ** 2 * dJsdp ** 2 - mu / Js * d2Jdp2
+         + lmbda * (Js - 1.) * d2Jdp2 + lmbda * dJsdp ** 2)
     return L
-    
+
 
 mesh = MeshTet()
 mesh.refine(2)
@@ -142,15 +166,27 @@ du = zeros(basis["u"].N)
 dp = zeros(basis["p"].N)
 stretch_ = 1.
 
-dofsold = [
-    basis["u"].find_dofs({"left":mesh.facets_satisfying(lambda x: x[0] < 1.e-6)}, skip=["u^2", "u^3"]),
-    basis["u"].find_dofs({"bottom":mesh.facets_satisfying(lambda x: x[1] < 1.e-6)}, skip=["u^1", "u^3"]),
-    basis["u"].find_dofs({"back":mesh.facets_satisfying(lambda x: x[2] < 1.e-6)}, skip=["u^1", "u^2"]),
-    basis["u"].find_dofs({"front":mesh.facets_satisfying(lambda x: npabs(x[2]-1.) < 1.e-6 )}, skip=["u^1", "u^2"])
+ddofs = [
+    basis["u"].find_dofs(
+        {"left": mesh.facets_satisfying(lambda x: x[0] < 1.e-6)},
+        skip=["u^2", "u^3"]
+    ),
+    basis["u"].find_dofs(
+        {"bottom": mesh.facets_satisfying(lambda x: x[1] < 1.e-6)},
+        skip=["u^1", "u^3"]
+    ),
+    basis["u"].find_dofs(
+        {"back": mesh.facets_satisfying(lambda x: x[2] < 1.e-6)},
+        skip=["u^1", "u^2"]
+    ),
+    basis["u"].find_dofs(
+        {"front": mesh.facets_satisfying(lambda x: npabs(x[2] - 1.) < 1e-6)},
+        skip=["u^1", "u^2"]
+    )
 ]
 
 dofs = {}
-for dof in dofsold:
+for dof in ddofs:
     dofs.update(dof)
 
 du[dofs["left"].all()] = 0.
@@ -163,33 +199,39 @@ I = hstack((
     basis["u"].N + arange(basis["p"].N)
 ))
 
+
 @LinearForm
 def a1(v, w):
     return einsum("ij...,ij...", F1(w), grad(v))
+
 
 @LinearForm
 def a2(v, w):
     return F2(w) * v
 
+
 @BilinearForm
 def b11(u, v, w):
     return einsum("ijkl...,ij...,kl...", A11(w), grad(u), grad(v))
+
 
 @BilinearForm
 def b12(u, v, w):
     return einsum("ij...,ij...", A12(w), grad(v)) * u
 
+
 @BilinearForm
 def b22(u, v, w):
     return A22(w) * u * v
+
 
 for itr in range(12):
     uv = basis["u"].interpolate(du)
     pv = basis["p"].interpolate(dp) 
 
-    K11 = asm(b11, basis["u"], basis["u"], disp=uv, press = pv)
-    K12 = asm(b12, basis["p"], basis["u"], disp=uv, press = pv)
-    K22 = asm(b22, basis["p"], basis["p"], disp=uv, press = pv)
+    K11 = asm(b11, basis["u"], basis["u"], disp=uv, press=pv)
+    K12 = asm(b12, basis["p"], basis["u"], disp=uv, press=pv)
+    K22 = asm(b22, basis["p"], basis["p"], disp=uv, press=pv)
     f = concatenate((
         asm(a1, basis["u"], disp=uv, press=pv),
         asm(a2, basis["p"], disp=uv, press=pv)
@@ -209,13 +251,12 @@ for itr in range(12):
         break
 
 
-
 if __name__ == "__main":
     from meshio import xdmf, Mesh
     xdmf.write("example36_results.xdmf",
         Mesh(mesh.p, cells={"tetra": mesh.t.T},
         point_data={
-            "u":du[basis["u"].nodal_dofs].T,
-            "p":dp[basis["p"].nodal_dofs].T
+            "u": du[basis["u"].nodal_dofs].T,
+            "p": dp[basis["p"].nodal_dofs].T
         })
     )
