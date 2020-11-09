77a51724a9354c13e3550f8651592fc01d4ca269
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-11-10 01:08:23 +0200

    Fix ElementQuad0 for FacetBasis

--- skfem/element/element_quad/element_quad0.py
+++ skfem/element/element_quad/element_quad0.py
@@ -15,6 +15,6 @@ class ElementQuad0(ElementH1):
 
     def lbasis(self, X, i):
         if i == 0:
-            return np.ones(X.shape[1]), np.zeros_like(X)
+            return np.ones(X.shape[1:]), np.zeros_like(X)
         else:
             self._index_error()
