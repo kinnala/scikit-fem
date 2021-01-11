279b3618a0033ecbed758d3605783f0d56b52273
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-01-11 11:12:31 +0200

    Make doctest more idiomatic

--- skfem/assembly/basis/basis.py
+++ skfem/assembly/basis/basis.py
@@ -95,8 +95,7 @@ class Basis:
         Facets can be queried from :class:`~skfem.mesh.Mesh` objects:
 
         >>> from skfem import MeshTri
-        >>> m = MeshTri()
-        >>> m = m.refine()
+        >>> m = MeshTri().refine()
         >>> m.facets_satisfying(lambda x: x[0] == 0)
         array([1, 5])
 
