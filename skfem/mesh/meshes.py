6f8a7b4df4f9696abcd1bafb40c173424f63f8db
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-02-21 01:20:33 +0200

    Simplify the structure

--- skfem/mesh/meshes.py
+++ skfem/mesh/meshes.py
@@ -1,23 +1,52 @@
 import warnings
+from typing import Type
 
 import numpy as np
 from numpy import ndarray
 
 from .geometry import Geometry
+from ..element import (Element, ElementTriP1, ElementQuad1, ElementTriP2,
+                       ElementQuad2, ElementTetP1, ElementHex1)
 
 
 class BaseMesh:
 
     geom: Geometry
     boundaries = None
+    _elem: Type
 
-    def __init__(self, *args, **kwargs):
-        self.geom = Geometry(*args, **kwargs)
+    def __init__(self, p=None, t=None):
+
+        if t.shape[0] > self._elem.refdom.nnodes:
+            m = self._elem.refdom.nnodes
+            _t = t[:m]
+            uniq, ix = np.unique(_t, return_inverse=True)
+            rest = np.setdiff1d(np.arange(np.max(t) + 1, dtype=np.int64),
+                                uniq)
+            _p = p[:, uniq]
+            p = np.hstack((_p, p[:, rest]))
+            __t = (np.arange(len(uniq), dtype=np.int64)[ix]
+                   .reshape(_t.shape))
+        else:
+            __t = t
+        self.geom = Geometry(
+            self._elem.refdom.dim(),
+            __t,
+            self._elem(),
+            p,
+        )
+        if t.shape[0] > self._elem.refdom.nnodes:
+            self.doflocs[:, t[m:].flatten('F')] =\
+                self.doflocs[:, self.dofs.element_dofs[m:].flatten('F')]
 
     @classmethod
     def load(cls, filename):
         from skfem.io.meshio import from_file
         return from_file(filename)
+
+    @classmethod
+    def init_refdom(cls):
+        return cls(*cls._elem.refdom.init_refdom())
 
     def __getattr__(self, item):
         return getattr(self.geom, item)
@@ -36,50 +65,33 @@ class BaseMesh2D(BaseMesh):
 
 class MeshTri1(BaseMesh2D):
 
-    def __init__(self, p=None, t=None):
-        from skfem.element import ElementTriP1
-        super(MeshTri1, self).__init__(2, t, ElementTriP1(), p)
+    _elem = ElementTriP1
 
 
 class MeshQuad1(BaseMesh2D):
 
-    def __init__(self, p=None, t=None):
-        from skfem.element import ElementQuad1
-        super(MeshQuad1, self).__init__(2, t, ElementQuad1(), p)
+    _elem = ElementQuad1
 
 
 class MeshTri2(BaseMesh2D):
 
-    def __init__(self, p=None, t=None):
-        from skfem.element import ElementTriP2
-        _t = t[:3]
-        uniq, ix = np.unique(_t, return_inverse=True)
-        rest = np.setdiff1d(np.arange(np.max(t) + 1, dtype=np.int64),
-                            uniq)
-        _p = p[:, uniq]
-        _p = np.hstack((_p, p[:, rest]))
-        __t = (np.arange(len(uniq), dtype=np.int64)[ix]
-               .reshape(_t.shape))
-        super(MeshTri2, self).__init__(2, __t, ElementTriP2(), _p)
-        self.doflocs[:, t[3:].flatten('F')] = self.doflocs[:, self.dofs.element_dofs[3:].flatten('F')]
+    _elem = ElementTriP2
+
+    @classmethod
+    def init_refdom(cls):
+        return MeshTri1(*MeshTri1._elem.refdom.init_refdom())
 
 
 class MeshQuad2(BaseMesh2D):
 
-    def __init__(self, p=None, t=None):
-        from skfem.element import ElementQuad2
-        super(MeshQuad2, self).__init__(2, t, ElementQuad2(), p)
+    _elem = ElementQuad2
 
 
 class MeshTet1(BaseMesh):
 
-    def __init__(self, p=None, t=None):
-        from skfem.element import ElementTetP1
-        super(MeshTet1, self).__init__(3, t, ElementTetP1(), p)
+    _elem = ElementTetP1
 
 
 class MeshHex1(BaseMesh2D):
 
-    def __init__(self, p=None, t=None):
-        from skfem.element import ElementHex1
-        super(MeshHex1, self).__init__(3, t, ElementHex1(), p)
+    _elem = ElementHex1
