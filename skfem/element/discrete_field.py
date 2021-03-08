60bd7c7a6e38f3ae18bf265511b3b5dfe8d519b3
Topi Kuutela <topi.kuutela@aalto.fi>
2021-03-08 10:32:17 +0200

    Add all operator overloads to discrete_field
    Signed-off-by: Topi Kuutela <topi.kuutela@aalto.fi>

--- skfem/element/discrete_field.py
+++ skfem/element/discrete_field.py
@@ -21,13 +21,48 @@ class DiscreteField(NamedTuple):
     def __array__(self):
         return self.value
 
+    def __add__(self, other):
+        if isinstance(other, DiscreteField):
+            return self.value + other.value
+        return self.value + other
+
+    def __sub__(self, other):
+        if isinstance(other, DiscreteField):
+            return self.value - other.value
+        return self.value - other
+
     def __mul__(self, other):
         if isinstance(other, DiscreteField):
             return self.value * other.value
         return self.value * other
 
+    def __truediv__(self, other):
+        if isinstance(other, DiscreteField):
+            return self.value / other.value
+        return self.value / other
+
+    def __pow__(self, other):
+        if isinstance(other, DiscreteField):
+            return self.value ** other.value
+        return self.value ** other
+
+    def __neg__(self):
+        return -self.value
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rsub__(self, other):
+        return other - self.value
+
     def __rmul__(self, other):
         return self.__mul__(other)
+
+    def __rtruediv__(self, other):
+        return other / self.value
+
+    def __rpow__(self, other):
+        return other ** self.value
 
     def _split(self):
         """Split all components based on their first dimension."""
