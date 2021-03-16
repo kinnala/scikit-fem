a804330d581b60b6d8d0c986e4eea2e81c20b7a1
Topi Kuutela <topi.kuutela@aalto.fi>
2021-03-16 09:45:07 +0200

    Cache element_dofs property in basis
    This improves performance in almost all cases.
    
    Signed-off-by: Topi Kuutela <topi.kuutela@aalto.fi>

---
+++ skfem/generic_utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class HashableNdArray(np.ndarray):
+    # Immutable np.ndarray with hashing support. Intended for enabling caching
+    # in other functions as default Python caching requires hashability of
+    # function arguments.
+    # Note that if HashableNdArray is used to wrap an existing np.ndarray,
+    # the HashableNdArray becomes a view of the underlying array. Therefore
+    # modifying the underlying array is likely to break things.
+
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 1 and len(kwargs) == 0 and \
+                isinstance(args[0], np.ndarray):
+            obj = np.asarray(args[0]).view(cls)
+            obj._hash = None
+            obj.flags.writeable = False
+        else:
+            obj = super(HashableNdArray, cls).__new__(cls, *args, **kwargs)
+        obj.flags.writeable = False
+        return obj
+
+    def __hash__(self):
+        if self._hash is None:
+            self._hash = hash(self.tobytes())
+        return self._hash
+
+    def __eq__(self, other):
+        if isinstance(other, HashableNdArray):
+            return (self.__array__() == other.__array__()).all()
