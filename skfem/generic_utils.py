6c20156e9acca534335f0ec7ddcb4e9733012549
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-03-20 00:12:08 +0200

    Update docstring

--- skfem/generic_utils.py
+++ skfem/generic_utils.py
@@ -2,16 +2,19 @@ import numpy as np
 
 
 class HashableNdArray(np.ndarray):
-    # Immutable np.ndarray with hashing support. Intended for enabling caching
-    # in other functions as default Python caching requires hashability of
-    # function arguments.
-    # Note that if HashableNdArray is used to wrap an existing np.ndarray,
-    # the HashableNdArray becomes a view of the underlying array. Therefore
-    # modifying the underlying array is likely to break things.
+    """Immutable ndarray with hasing support.
+
+    Intended for enabling caching in other functions as default Python caching
+    requires hashability of function arguments.  Note that if HashableNdArray
+    is used to wrap an existing ndarray, the HashableNdArray becomes a view of
+    the underlying array. Therefore modifying the underlying array is likely to
+    break things.
+
+    """
 
     def __new__(cls, *args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and \
-                isinstance(args[0], np.ndarray):
+        if len(args) == 1 and len(kwargs) == 0 and\
+           isinstance(args[0], np.ndarray):
             obj = np.asarray(args[0]).view(cls)
             obj._hash = None
             obj.flags.writeable = False
