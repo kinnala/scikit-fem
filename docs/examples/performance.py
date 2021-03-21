9f1957a255c57729f907afbf57469252f57b026f
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-03-21 10:13:01 +0200

    Modify performance.py so that it's comparable to the earlier results

--- docs/examples/performance.py
+++ docs/examples/performance.py
@@ -18,26 +18,31 @@ print('| Degrees-of-freedom | Assembly (s) | Linear solve (s) |')
 print('| --- | --- | --- |')
 
 
+def assembler(m):
+    basis = InteriorBasis(m, ElementTetP1())
+    return (
+        asm(laplace, basis),
+        asm(unit_load, basis),
+    )
+
+
 for k in range(6, 21):
 
     m = pre(int(2 ** (k / 3)))
 
-    def assembler(m):
-        basis = InteriorBasis(m, ElementTetP1())
-        return (asm(laplace, basis),
-                asm(unit_load, basis),
-                m.boundary_nodes())
-
-    A, b, D = assembler(m)
-
     assemble_time = timeit(lambda: assembler(m), number=1)
 
-    def solver(A, b):
-        return solve(*condense(A, b, D=D))
+    A, b = assembler(m)
+    D = m.boundary_nodes()
 
     if A.shape[0] > 1e5:
         solve_time = np.nan
     else:
-        solve_time = timeit(lambda: solver(A, b), number=1)
+        solve_time = timeit(lambda: solve(*condense(A, b, D=D)), number=1)
 
     print('| {} | {:.5f} | {:.5f} |'.format(len(b), assemble_time, solve_time))
+
+    del A
+    del b
+    del D
+    del m
