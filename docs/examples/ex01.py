5fc95cc873e21ff61c764bd04ce25e59ebc5273c
Nico Schlömer <nico.schloemer@gmail.com>
2021-06-04 14:41:13 +0200

    make ex01 more explicit

--- docs/examples/ex01.py
+++ docs/examples/ex01.py
@@ -1,20 +1,41 @@
 from skfem import *
-from skfem.models import laplace, unit_load
+from skfem.helpers import dot, grad
 
+# create the mesh
 m = MeshTri().refined(4)
+# or, with your own points and cells:
+# m = MeshTri(points, cells)
 
 e = ElementTriP1()
 basis = InteriorBasis(m, e)
 
-A = asm(laplace, basis)
-b = asm(unit_load, basis)
+# this method could also be imported from skfem.models.laplace
+@BilinearForm
+def laplace(u, v, _):
+    return dot(grad(u), grad(v))
 
-x = solve(*condense(A, b, I=m.interior_nodes()))
+
+# this method could also be imported from skfem.models.unit_load
+@LinearForm
+def rhs(v, _):
+    return 1.0 * v
+
+A = asm(laplace, basis)
+b = asm(rhs, basis)
+# or:
+# A = laplace.assemble(basis)
+# b = rhs.assemble(basis)
+
+# enforce Dirichlet boundary conditions
+A, b = enforce(A, b, D=m.boundary_nodes())
+
+# solve -- can be anything that takes a sparse matrix and a right-hand side
+x = solve(A, b)
 
 if __name__ == "__main__":
     from os.path import splitext
     from sys import argv
     from skfem.visuals.matplotlib import plot, savefig
-   
+
     plot(m, x, shading='gouraud', colorbar=True)
     savefig(splitext(argv[0])[0] + '_solution.png')
