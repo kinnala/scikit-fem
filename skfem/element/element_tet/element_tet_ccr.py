30b6d7c356a57487d7d92dca58c8f61ad1b83e9f
Bhavesh Shrimali <bhavesh.shrimali@gmail.com>
2021-06-18 12:24:52 -0500

    remote whitespaces and adjust alignment

--- skfem/element/element_tet/element_tet_ccr.py
+++ skfem/element/element_tet/element_tet_ccr.py
@@ -35,41 +35,41 @@ class ElementTetCCR(ElementH1):
 
         if i == 0:  # at (0,0,0)
             phi = (x + y + z - 1)*(4*x*y*z - 3*x*y - 3*x*z + 2*x - 3*y*z +
-             2*y + 2*z - 1)
+                   2*y + 2*z - 1)
             dphi = np.array([
                 8*x*y*z - 6*x*y - 6*x*z + 4*x + 4*y**2*z - 3*y**2 + 4*y*z**2 -
-                 13*y*z + 7*y - 3*z**2 + 7*z - 3,
+                   13*y*z + 7*y - 3*z**2 + 7*z - 3,
                 4*x**2*z - 3*x**2 + 8*x*y*z - 6*x*y + 4*x*z**2 - 13*x*z + 7*x -
-                 6*y*z + 4*y - 3*z**2 + 7*z - 3,
+                   6*y*z + 4*y - 3*z**2 + 7*z - 3,
                 4*x**2*y - 3*x**2 + 4*x*y**2 + 8*x*y*z - 13*x*y - 6*x*z + 7*x -
-                 3*y**2 - 6*y*z + 7*y + 4*z - 3
+                   3*y**2 - 6*y*z + 7*y + 4*z - 3
             ])
         elif i == 1:  # at (1,0,0)
             phi = 3 * x * (y * z + (y + z) * (-x - y - z + 1)) + x * (
                   2 * x - 4 * y * z * (-x - y - z + 1) - 1)
             dphi = np.array([
-                -3*x*(y + z) + 2*x*(2*y*z + 1) + 2*x + 4*y*z*(x + y + z - 1) + 
+                -3*x*(y + z) + 2*x*(2*y*z + 1) + 2*x + 4*y*z*(x + y + z - 1) +
                 3*y*z - 3*(y + z)*(x + y + z - 1) - 1,
                 x*(4*z - 3)*(x + 2*y + z - 1),
                 x*(4*y - 3)*(x + y + 2*z - 1)
             ])
         elif i == 2:  # at (0,1,0)
             phi = y*(4*x*z*(x + y + z - 1) + 3*x*z + 2*y -
-             3*(x + z)*(x + y + z - 1) - 1)
+                   3*(x + z)*(x + y + z - 1) - 1)
             dphi = np.array([
                 y*(4*z - 3)*(2*x + y + z - 1),
                 4*x*z*(x + y + z - 1) + 3*x*z - 3*y*(x + z) + 2*y*(2*x*z + 1) +
-                 2*y - 3*(x + z)*(x + y + z - 1) - 1,
+                   2*y - 3*(x + z)*(x + y + z - 1) - 1,
                 y*(4*x - 3)*(x + y + 2*z - 1)
             ])
         elif i == 3:  # at (0,0,1)
             phi = z*(4*x*y*(x + y + z - 1) + 3*x*y +
-             2*z - 3*(x + y)*(x + y + z - 1) - 1)
+                   2*z - 3*(x + y)*(x + y + z - 1) - 1)
             dphi = np.array([
                 z*(4*y - 3)*(2*x + y + z - 1),
                 z*(4*x - 3)*(x + 2*y + z - 1),
                 4*x*y*(x + y + z - 1) + 3*x*y - 3*z*(x + y) + 2*z*(2*x*y + 1) +
-                 2*z - 3*(x + y)*(x + y + z - 1) - 1
+                   2*z - 3*(x + y)*(x + y + z - 1) - 1
             ])
         elif i == 4:  # between (0,1)
             phi = -4*x*(x + y + z - 1)*(8*y*z - 3*y - 3*z + 1)
@@ -92,7 +92,7 @@ class ElementTetCCR(ElementH1):
                 4*(-x - 2*y - z + 1)*(8*x*z - 3*x - 3*z + 1),
                 4*y*(-8*x*z + 3*x + 3*z - (8*x - 3)*(x + y + z - 1) - 1)
             ])
-        elif i == 7:  # between (0,3) 
+        elif i == 7:  # between (0,3)
             phi = -4*z*(x + y + z - 1)*(8*x*y - 3*x - 3*y + 1)
             dphi = np.array([
                 4*z*(-8*x*y + 3*x + 3*y - (8*y - 3)*(x + y + z - 1) - 1),
@@ -146,7 +146,7 @@ class ElementTetCCR(ElementH1):
             dphi = np.array([
                 256*y*z*(-2*x - y - z + 1),
                 256*x*z*(-x - 2*y - z + 1),
-                256*x*y*(-x - y - 2*z + 1)                
+                256*x*y*(-x - y - 2*z + 1)
             ])
         else:
             self._index_error()
