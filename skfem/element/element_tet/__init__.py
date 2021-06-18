a558173b51a82fce9ae18b171f5d7518b4de8de0
Bhavesh Shrimali <bhavesh.shrimali@gmail.com>
2021-06-18 11:51:22 -0500

    add ElementTetCCR and ElementTriCCR

--- skfem/element/element_tet/__init__.py
+++ skfem/element/element_tet/__init__.py
@@ -6,3 +6,4 @@ from .element_tet_n0 import ElementTetN0  # noqa
 from .element_tet_mini import ElementTetMini  # noqa
 from .element_tet_cr import ElementTetCR  # noqa
 from .element_tet_dg import ElementTetDG  # noqa
+from .element_tet_ccr import ElementTetCCR # noqa
