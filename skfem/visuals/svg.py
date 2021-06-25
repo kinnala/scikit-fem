e92d1a4616890ceb330c62a629122bac1f3e521b
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-06-25 22:55:14 +0300

    Add colorbar etc

--- skfem/visuals/svg.py
+++ skfem/visuals/svg.py
@@ -44,18 +44,19 @@ def draw_mesh2d(m: Mesh2D, **kwargs) -> str:
     else:
         height = width * (maxy - miny) / (maxx - minx)
     stroke = kwargs["stroke"] if "stroke" in kwargs else 1
+    color = kwargs["color"] if "color" in kwargs else "#7856FA"
     sx = (width - 2 * stroke) / (maxx - minx)
     sy = (height - 2 * stroke) / (maxy - miny)
     p[0] = sx * (p[0] - minx) + stroke
     p[1] = sy * (maxy - p[1]) + stroke
     template = ("""<line x1="{}" y1="{}" x2="{}" y2="{}" """
-                """style="stroke:#7856FA;stroke-width:{}"/>""")
+                """style="stroke:{};stroke-width:{}"/>""")
     lines = ""
     for s, t, u, v in zip(p[0, facets[0]],
                           p[1, facets[0]],
                           p[0, facets[1]],
                           p[1, facets[1]]):
-        lines += template.format(s, t, u, v, stroke)
+        lines += template.format(s, t, u, v, color, stroke)
     return ("""<svg xmlns="http://www.w3.org/2000/svg" version="1.1" """
             """width="{}" height="{}">{}</svg>""").format(width, height, lines)
 
@@ -100,7 +101,7 @@ def plot_mesh_tri(m: MeshTri, x: ndarray, **kwargs) -> None:
     p[0] = sx * (p[0] - minx) + stroke
     p[1] = sy * (maxy - p[1]) + stroke
     template = ("""<polygon points="{},{} {},{} {},{}" """
-                """style="fill:rgb(0, {}, 0);stroke:white;stroke-width:{}" />""")
+                """style="fill:rgb(100, {}, {});" />""")
     elems = ""
     for ix, tri in enumerate(zip(p[0, t[0]],
                                  p[1, t[0]],
@@ -108,13 +109,36 @@ def plot_mesh_tri(m: MeshTri, x: ndarray, **kwargs) -> None:
                                  p[1, t[1]],
                                  p[0, t[2]],
                                  p[1, t[2]])):
-        elems += template.format(*tri, int((x[t[:, ix]].mean() - minval) / (maxval - minval) * 255), 0 * stroke)
+        color = int((x[t[:, ix]].mean() - minval) / (maxval - minval) * 255)
+        elems += template.format(*tri, color, 255 - color)
+    elems += draw_mesh2d(m, boundaries_only=True, color='black')
     return ("""<svg xmlns="http://www.w3.org/2000/svg" version="1.1" """
-            """width="{}" height="{}" shape-rendering="crispEdges">{}</svg>""").format(width, height, elems)
+            """width="{}" height="{}" shape-rendering="crispEdges">"""
+            """<defs>"""
+            """<linearGradient id="cbar" x1="0%" y1="0%" x2="0%" y2="100%">"""
+            """<stop offset="0%" style="stop-color:rgb(100,255,0);" />"""
+            """<stop offset="100%" style="stop-color:rgb(100,0,255);" />"""
+            """</linearGradient>"""
+            """</defs>"""
+            """{}"""
+            """<polygon points="{},{} {},{} {},{} {},{}" """
+            """fill="url(#cbar)" style="stroke:black;stroke-width:{}" />"""
+            """</svg>""").format(width + 30,
+                                 height,
+                                 elems,
+                                 width + 29,
+                                 height - 1,
+                                 width + 29,
+                                 1,
+                                 width + 10,
+                                 1,
+                                 width + 10,
+                                 height - 1,
+                                 stroke)
 
 
 @plot.register(InteriorBasis)
 def plot_basis(ib: InteriorBasis, x: ndarray, **kwargs) -> str:
-    nrefs = kwargs["nrefs"] if "nrefs" in kwargs else 2
+    nrefs = kwargs["nrefs"] if "nrefs" in kwargs else 0
     m, X = ib.refinterp(x, nrefs=nrefs)
     return plot(m, X, **kwargs)
