4c35c4f927d448cff7b321c7db12da17fba48a0b
G. D. McBain <gdmcbain@protonmail.com>
2021-08-05 16:55:27 +1000

    ...and deserialize, fixes #261

--- docs/examples/ex_261.py
+++ docs/examples/ex_261.py
@@ -5,7 +5,7 @@ import numpy as np
 import meshio
 import skfem as fe
 from skfem.io.json import from_file
-from skfem.io.meshio import TYPE_MESH_MAPPING
+from skfem.io.meshio import MESH_TYPE_MAPPING, TYPE_MESH_MAPPING
 
 
 def to_meshio(mesh: fe.Mesh) -> meshio.Mesh:
@@ -27,9 +27,41 @@ def to_meshio(mesh: fe.Mesh) -> meshio.Mesh:
     )
 
 
+def from_meshio(mio: meshio.Mesh) -> fe.Mesh:
+    p = mio.points.T
+    t = mio.cells[0].data.T
+    mesh_type = MESH_TYPE_MAPPING[mio.cells[0].type]
+    mtmp = mesh_type(p, t)
+    boundaries = {}
+    subdomains = {}
+    for name, data in mio.cell_data.items():
+        subnames = name.split(":")
+        if subnames[0] != "skfem":
+            continue
+        if subnames[1] == "subdomain":
+            subdomains[subnames[2]] = np.nonzero(data[0])[0]
+        elif subnames[1] == "boundary":
+            boundaries[subnames[2]] = mtmp.t2f[
+                (2 ** np.arange(t.shape[0]))[:, None] & data[0] > 0
+            ]
+    return mesh_type(p, t, boundaries, subdomains)
+
+
 meshes = Path(__file__).parent / "meshes"
 for name, mesh in [
     ("disk", from_file(meshes / "disk.json")),
     ("beams", fe.Mesh.load(meshes / "beams.msh")),
 ]:
-    meshio.write(Path(__file__).with_name(f"{name}.vtk"), to_meshio(mesh))
+    mio = to_meshio(mesh)
+    meshio.write(Path(__file__).with_name(f"{name}.vtk"), mio)
+    mesh2 = from_meshio(mio)
+    print("\n", name)
+    print("As read:", mesh)
+    print("To meshio:", mio)
+    print("Reconverted:", mesh2, "\n")
+    for subdomain_name, subdomain in mesh.subdomains.items():
+        assert np.setxor1d(subdomain, mesh2.subdomains[subdomain_name]).size == 0
+        print(subdomain_name)
+    for boundary_name, boundary in mesh.boundaries.items():
+        assert np.setxor1d(boundary, mesh2.boundaries[boundary_name]).size == 0
+        print(boundary_name)
