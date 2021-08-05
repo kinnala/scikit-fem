2827899afcbac06a8b2805d63fda69dab935d0bf
G. D. McBain <gdmcbain@protonmail.com>
2021-08-05 14:19:31 +1000

    boundary facet indicators #261

--- docs/examples/ex_261.py
+++ docs/examples/ex_261.py
@@ -9,12 +9,23 @@ from skfem.io.meshio import TYPE_MESH_MAPPING
 
 
 def to_meshio(mesh: fe.Mesh) -> meshio.Mesh:
-    cell_data = {
+    subdomains = {
         f"skfem:subdomain:{name}": [
             np.isin(np.arange(mesh.t.shape[1]), subdomain).astype(int)
         ]
         for name, subdomain in mesh.subdomains.items()
     }
+    boundaries = {}
+    for name, boundary in mesh.boundaries.items():
+        name = f"skfem:boundary:{name}"
+        b = np.isin(mesh.t2f, boundary)
+        if b.sum(0).max() > 1:  # a cell has more than one facet on boundary
+            raise NotImplementedError
+        boundaries[name] = [np.full(mesh.t.shape[1:], mesh.t.shape[0])]  # sentinel
+        bmask = np.nonzero(b)
+        boundaries[name][0][bmask[1]] = bmask[0][1]
+
+    cell_data = subdomains | boundaries
     return meshio.Mesh(
         mesh.p.T, [(TYPE_MESH_MAPPING[type(mesh)], mesh.t.T)], cell_data=cell_data
     )
