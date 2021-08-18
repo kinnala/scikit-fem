877073479814fcc6c8d3a73581d8a83c665a2c6e
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-08-18 18:56:12 +0300

    Make FacetBasis a simple alias to BoundaryFacetBasis

--- skfem/assembly/basis/__init__.py
+++ skfem/assembly/basis/__init__.py
@@ -10,16 +10,4 @@ from .mortar_facet_basis import MortarFacetBasis  # noqa
 Basis = CellBasis
 InteriorBasis = CellBasis  # backwards compatibility
 ExteriorFacetBasis = BoundaryFacetBasis  # backwards compatibility
-
-
-def FacetBasis(*args, side=None, **kwargs):
-    """alias of :class:`~skfem.assembly.BoundaryFacetBasis`"""
-    if side is None:
-        return BoundaryFacetBasis(*args, **kwargs)
-    warnings.warn("Initializing FacetBasis using the keyword argument side "
-                  "is deprecated. Use InteriorFacetBasis or MortarFacetBasis "
-                  "instead.", DeprecationWarning)
-    if 'mapping' in kwargs:
-        if hasattr(kwargs['mapping'], 'helper_to_orig'):
-            return MortarFacetBasis(*args, side=side, **kwargs)
-    return InteriorFacetBasis(*args, side=side, **kwargs)
+FacetBasis = BoundaryFacetBasis
