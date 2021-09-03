8889c4fe9dfcde1af9e6a78cf1891a1fc1346bd0
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-09-04 00:35:15 +0300

    Try always using dask

--- skfem/multi.py
+++ skfem/multi.py
@@ -24,8 +24,7 @@ def MultiBasis(mesh,
                mapping=None,
                intorder=None,
                elements=None,
-               quadrature=None,
-               use_dask=False):
+               quadrature=None):
 
     from itertools import cycle
 
@@ -43,19 +42,31 @@ def MultiBasis(mesh,
 
     params = [arg for arg in zip(mesh, cycle(elem))]
 
-    if use_dask:
+    try:
+        import dask
+        dask.config.set(scheduler='threading')
         import dask.bag as db
         bag = db.from_sequence(params)
         return bag.map(lambda p: Basis(*p))
+    except Exception:
+        pass
 
     return [Basis(*p) for p in params]
 
 
-def multiasm(form, bases):
+def multiasm(form, bases, compute=True):
 
-    if not isinstance(bases, list):
+    try:
+        if isinstance(bases, list):
+            import dask.bag as db
+            bases = db.from_sequence(bases)
+
         c = bases.map(lambda basis: form.coo_data(basis)).sum()
+        if not compute:
+            return c
         out = c.compute(scheduler='threading')
         return out.tocsr()
+    except Exception:
+        pass
 
     return sum(map(lambda basis: form.coo_data(basis), bases)).tocsr()
