b63289ef39d506f8b17ca8b59cca9ad98682a663
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-09-17 13:28:59 +0300

    Loosen the delta for reproducibility

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -363,7 +363,7 @@ class TestEx42(TestCase):
     def runTest(self):
         import docs.examples.ex42 as ex
 
-        self.assertAlmostEqual(ex.x.max(), 0.0009824131638261542)
+        self.assertAlmostEqual(ex.x.max(), 0.0009824131638261542, delta=1e-5)
 
 
 if __name__ == '__main__':
