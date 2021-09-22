a40ac1a7af212f6f832e1ea0f973aa736354aa68
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-09-22 15:38:36 +0300

    Use UGrid in skfem.visuals.vedo

--- skfem/visuals/vedo.py
+++ skfem/visuals/vedo.py
@@ -5,16 +5,16 @@ def draw(m, backend=False, **kwargs):
     """Visualize meshes."""
     import vedo
     vedo.embedWindow(backend)
-    from vedo import Plotter
+    from vedo import Plotter, UGrid, show
     vp = Plotter()
-    tetmesh = None
+    grid = None
     with tempfile.NamedTemporaryFile() as tmp:
         m.save(tmp.name + '.vtk',
                encode_cell_data=False,
                encode_point_data=True,
                **kwargs)
-        tetmesh = vp.load(tmp.name + '.vtk')
+        grid = UGrid(tmp.name + '.vtk')
         # save these for further use
-        tetmesh.show = lambda: vp.show([tetmesh]).close()
-        tetmesh.plotter = vp
-    return tetmesh
+        grid.show = lambda: vp.show([grid.tomesh()]).close()
+        grid.plotter = vp
+    return grid
