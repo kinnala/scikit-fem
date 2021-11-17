f54bd186ecb8b46ad9ff36c276d5c432c4acb070
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-11-17 17:36:14 +0200

    Introduce ElementDG which works for all reference domains

--- skfem/element/element_hex/__init__.py
+++ skfem/element/element_hex/__init__.py
@@ -2,4 +2,3 @@ from .element_hex0 import ElementHex0  # noqa
 from .element_hex1 import ElementHex1, ElementHex1DG  # noqa
 from .element_hex_s2 import ElementHexS2  # noqa
 from .element_hex2 import ElementHex2  # noqa
-from .element_hex_dg import ElementHexDG  # noqa
--- skfem/element/element_quad/__init__.py
+++ skfem/element/element_quad/__init__.py
@@ -2,6 +2,5 @@ from .element_quad0 import ElementQuad0  # noqa
 from .element_quad1 import ElementQuad1, ElementQuad1DG  # noqa
 from .element_quad2 import ElementQuad2  # noqa
 from .element_quad_s2 import ElementQuadS2  # noqa
-from .element_quad_dg import ElementQuadDG  # noqa
 from .element_quadp import ElementQuadP  # noqa
 from .element_quad_bfs import ElementQuadBFS  # noqa
