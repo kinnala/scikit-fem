0371ea848b4cf44f0d042cf751e014b0346225f4
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-11-20 11:58:36 +0200

    Fix mypy

--- skfem/__about__.py
+++ skfem/__about__.py
@@ -1,13 +1,18 @@
+legacy = False
 try:
     # Python 3.8+
     from importlib import metadata
 except ImportError:
     try:
-        import importlib_metadata as metadata
+        import importlib_metadata as metadata_legacy
+        legacy = True
     except ImportError:
         __version__ = "unknown"
 
 try:
-    __version__ = metadata.version("scikit-fem")
+    if legacy:
+        __version__ = metadata_legacy.version("scikit-fem")
+    else:
+        __version__ = metadata.version("scikit-fem")
 except Exception:
     __version__ = "unknown"
