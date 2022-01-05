81aee45040af833f951cabc80cf2178eeaa9a78e
George Gatling <george.gatling@nrl.navy.mil>
2022-01-04 22:06:02 -0500

    blacked

--- skfem/assembly/basis/__init__.py
+++ skfem/assembly/basis/__init__.py
@@ -1,9 +1,10 @@
 from .abstract_basis import AbstractBasis  # noqa
 from .cell_basis import CellBasis  # noqa
-from .facet_basis import FacetBasis  #noqa
+from .facet_basis import FacetBasis  # noqa
 from .boundary_facet_basis import BoundaryFacetBasis  # noqa
 from .interior_facet_basis import InteriorFacetBasis  # noqa
 from .mortar_facet_basis import MortarFacetBasis  # noqa
+
 # from .subdomain_facet_basis import SubdomainFacetBasis  #noqa
 
 # aliases
