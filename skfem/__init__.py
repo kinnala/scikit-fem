aff5f7cab6ad47021dd701575ac6afaf9dd1c35c
George Gatling <george.gatling@nrl.navy.mil>
2022-01-10 13:25:43 -0500

    fixed top level __init__.py

--- skfem/__init__.py
+++ skfem/__init__.py
@@ -6,30 +6,4 @@ from skfem.mapping import *  # noqa
 from skfem.element import *  # noqa
 from skfem.utils import *  # noqa
 
-from skfem.assembly import __all__ as all_assembly
-from skfem.mesh import __all__ as all_mesh
-from skfem.element import __all__ as all_element
-
 from .__about__ import __version__
-
-
-__all__ = all_mesh + all_assembly + all_element + [  # noqa
-    'MappingAffine',
-    'MappingIsoparametric',
-    'MappingMortar',
-    'adaptive_theta',
-    'build_pc_ilu',
-    'build_pc_diag',
-    'condense',
-    'enforce',
-    'penalize',
-    'project',
-    'projection',
-    'solve',
-    'solver_direct_scipy',
-    'solver_eigen_scipy',
-    'solver_eigen_scipy_sym',
-    'solver_iter_pcg',
-    'solver_iter_krylov',
-    '__version__',
-]
