d3b2c090dd3932edb1d386f481b2752a20a44dad
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-01-15 23:05:10 +0200

    Add a test for the simple cg solver

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,11 +3,11 @@ from unittest import TestCase
 import numpy as np
 from numpy.testing import assert_almost_equal
 
-from skfem.assembly import InteriorBasis
+from skfem.assembly import CellBasis
 from skfem.element import ElementTriP1
 from skfem.mesh import MeshTri
-from skfem.utils import projection, enforce
-from skfem.models import laplace, mass
+from skfem.utils import projection, enforce, condense, solve
+from skfem.models import laplace, mass, unit_load
 
 
 class InitializeScalarField(TestCase):
@@ -15,7 +15,7 @@ class InitializeScalarField(TestCase):
     def runTest(self):
 
         mesh = MeshTri().refined(5)
-        basis = InteriorBasis(mesh, ElementTriP1())
+        basis = CellBasis(mesh, ElementTriP1())
 
         def fun(X):
             x, y = X
@@ -38,7 +38,7 @@ class TestEnforce(TestCase):
 
         m = self.mesh
         e = ElementTriP1()
-        basis = InteriorBasis(m, e)
+        basis = CellBasis(m, e)
 
         A = laplace.assemble(basis)
         M = mass.assemble(basis)
@@ -50,3 +50,24 @@ class TestEnforce(TestCase):
 
         enforce(A, D=D, overwrite=True)
         assert_almost_equal(A.toarray(), np.eye(A.shape[0]))
+
+
+def test_simple_cg_solver():
+
+    m = MeshTri().refined(3)
+    basis = CellBasis(m, ElementTriP1())
+
+    A0 = laplace.coo_data(basis)
+    A1 = laplace.assemble(basis)
+
+    f = unit_load.assemble(basis)
+
+    D = m.boundary_nodes()
+
+    x1 = solve(*condense(A1, f, D=D))
+
+    f[D] = 0
+
+    x0 = A0.solve(f, D=D)
+
+    assert_almost_equal(x0, x1)
