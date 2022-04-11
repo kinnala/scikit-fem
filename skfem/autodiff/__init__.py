c1b96b2948c55126d7b0f7950988920add841b82
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-04-11 15:01:38 +0300

    Add diff

--- skfem/autodiff/__init__.py
+++ skfem/autodiff/__init__.py
@@ -1,5 +1,6 @@
-from skfem import DiscreteField
-from jax import linearize
+from skfem import DiscreteField, BilinearForm, LinearForm
+from skfem.assembly.form import Form
+from jax import linearize, jvp
 from jax.config import config
 
 
@@ -11,3 +12,20 @@ def derivative(fun, u0):
         raise NotImplementedError("Second argument must be DiscreteField.")
     y, DF = linearize(fun, u0.astuple)
     return y, lambda u: DF(u.astuple)
+
+
+def diff(F, u0):
+    if not isinstance(u0, DiscreteField):
+        raise NotImplementedError("Second argument must be DiscreteField.")
+
+    @BilinearForm
+    def DF(u, v, w):
+        F1 = lambda U: F(U, v, w)
+        return jvp(F1, (u0.astuple,), (u.astuple,))[1]
+
+    @LinearForm
+    def y(v, w):
+        F1 = lambda U: F(U, v, w)
+        return -F1(u0.astuple)
+
+    return y, DF
