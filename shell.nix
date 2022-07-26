72d61d2cbb75e7bbdbd571cdb64ad8f24c6537f5
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-07-26 19:53:46 +0000

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,22 +1,6 @@
 with import <nixpkgs> {};
 
-(let
-  pacopy016 = python38.pkgs.buildPythonPackage rec {
-    # not part of nixpkgs
-    pname = "pacopy";
-    version = "0.1.6";
-    format = "pyproject";
-    src = python38.pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "0rq5yfmq5516giyqsflm3bjirbfhigydd1vlxc554jnn13n3wisr";
-    };
-    doCheck = false;
-    meta = {
-      homepage = "https://github.com/nschloe/pacopy/";
-      description = "Numerical continuation in Python";
-    };
-  };
-in python38.withPackages (ps: with ps; [
+(python38.withPackages (ps: with ps; [
   numpy
   scipy
   meshio
@@ -29,6 +13,4 @@ in python38.withPackages (ps: with ps; [
   twine
   pep517
   pip
-  pacopy016
-  jupyter
 ])).env
