d11908f3a51fb5f510b4701f17afedbce5c391b3
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-10-21 15:04:32 +0300

    Solve for curl part only

--- docs/examples/ex46.py
+++ docs/examples/ex46.py
@@ -9,45 +9,37 @@ from skfem.helpers import *
 
 
 mesh = MeshTri().init_tensor(np.linspace(0, 1, 9),
-                             np.linspace(0, .5, 5)).refined()
-basis = Basis(mesh, ElementTriN0() * ElementTriP1())
+                             np.linspace(0, .5, 5)).refined(3)
+#basis = Basis(mesh, ElementTriN0() * ElementTriP1())
+basis = Basis(mesh, ElementTriN0())
 
-epsilon = 1
+epsilon = lambda x: 1. + 0. * x[0]
+# epsilon = lambda x: 3 * (x[1] < 0.25) + 1
 one_over_u_r = 1
 
 
-def rot(x):
-    return np.array([-x[1], x[0]])
+@BilinearForm
+def aform(E, v, w):
+    return one_over_u_r * curl(E) * curl(v)
 
 
 @BilinearForm
-def aform(Et, Ez, vt, vz, w):
-    return one_over_u_r * (curl(Et) * curl(vt) + dot(grad(Ez), grad(vz)))
-
-
-@BilinearForm
-def bform(Et, Ez, vt, vz, w):
-    return epsilon * (dot(Et, vt) + Ez * vz)
+def bform(E, v, w):
+    return epsilon(w.x) * dot(E, v)
 
 
 A = aform.assemble(basis)
 B = bform.assemble(basis)
 
-lam, x = solve(*condense(A, B, D=basis.get_dofs()),
-               solver=solver_eigen_scipy_sym(k=6))
+lams, xs = solve(*condense(A, B, D=basis.get_dofs()),
+                 solver=solver_eigen_scipy_sym(k=6))
 
 
-for itr in range(6):
-    (Et, Etbasis), (Ez, Ezbasis) = basis.split(x[:, itr])
-    Etbasis.plot(Et).show()
-    Ezbasis.plot(Ez, colorbar=True).show()
+for itr, lam in enumerate(lams):
+    if lam < 1e-8:
+        continue
+    basis.plot(xs[:, itr], colorbar=True).show()
 
-# basis.plot(basis.split_bases()[0],
-#            eigenVectors[basis.split_indices()[0], 0],
-#            ax=mesh.draw(boundaries_only=True),
-#            colorbar=True).show()
 
-# basis.plot(basis.split_bases()[1],
-#            eigenVectors[basis.split_indices()[1], 0],
-#            ax=mesh.draw(boundaries_only=True),
-#            colorbar=True).show()
+print('TE10 (calculated): {}'.format(lams[-1]))
+print('TE10 (analytical): {}'.format(np.pi ** 2))
