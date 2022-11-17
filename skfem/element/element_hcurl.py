188bbf8cee0a0d6f84b83067923ea5c297f44b1f
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-11-17 08:42:15 +0200

    Simplify orient using RefDom

--- skfem/element/element_hcurl.py
+++ skfem/element/element_hcurl.py
@@ -16,16 +16,12 @@ class ElementHcurl(Element):
                      else self.edge_dofs)
         ix = int(i / divide_by)
         if mapping.mesh.dim() == 2 and ix >= self.refdom.nfacets:
+            # no orientation required for interior DOFs => return 1
             return np.ones(mapping.mesh.t.shape[1], dtype=np.int64)
-        if mapping.mesh.dim() == 3 and mapping.mesh.t.shape[0] == 4:
-            t1 = [0, 1, 0, 0, 1, 2][ix]
-            t2 = [1, 2, 2, 3, 3, 3][ix]
-        elif mapping.mesh.dim() == 2 and mapping.mesh.t.shape[0] == 3:
-            t1 = [0, 1, 0][ix]
-            t2 = [1, 2, 2][ix]
-        elif mapping.mesh.dim() == 2 and mapping.mesh.t.shape[0] == 4:
-            t1 = [0, 1, 2, 3][ix]
-            t2 = [1, 2, 3, 0][ix]
+        if mapping.mesh.dim() == 3:
+            t1, t2 = mapping.mesh.refdom.edges[ix]
+        elif mapping.mesh.dim() == 2:
+            t1, t2 = mapping.mesh.refdom.facets[ix]
         else:
             raise NotImplementedError("The element type not supported.")
         return 1 - 2 * (mapping.mesh.t[t1] > mapping.mesh.t[t2])
