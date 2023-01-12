fd28966acfb031a091e51fbb828c07488fc29c48
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2023-01-11 19:25:35 -0800

    cleanup

--- skfem/multipoint.py
+++ skfem/multipoint.py
@@ -1,10 +1,46 @@
-# see https://quickfem.com/wp-content/uploads/IFEM.Ch08.pdf
-
-
-import numpy as np
 from scipy.sparse import csr_array
+import numpy as np
 
-if __name__ == '__main__':
+
+def multipoint(D, *Ixs, g=None):
+    if g is None:
+        g = np.zeros(D.N)
+    shape = D.N, D.N-len(D.flatten())
+
+    I_rows = np.arange(D.N-len(D.flatten()))
+    I_columns = np.setdiff1d(np.arange(D.N), D)
+
+    result = csr_array(
+        (np.ones((D.N-len(D.flatten()),)), (I_columns, I_rows)), shape=shape
+    )
+
+    for Ix in Ixs:
+        I, x = Ix
+        result += csr_array(
+            (x, (D.flatten(), [I_columns.tolist().index(i) for i in I.flatten()])), shape=shape
+        )
+
+    if len(np.nonzero(g[I_columns])[0]):
+        raise ValueError('g not correct, defines not removed DOFs')
+    return result, g, np.setdiff1d(np.arange(D.N), D)
+
+
+def combine(M1, M2):
+    T1, g1, I1 = M1
+    T2, g2, I2 = M2
+
+    return T1[:, np.isin(I1, I2)]@T2[np.intersect1d(I1, I2), :][:, np.isin(I2, I1)], g1 + g2
+
+
+def apply(A, f, T, g):
+    return T.T@A@T, T.T@(f-A@g)
+
+
+def restore(y, T, g):
+    return T@y+g
+
+
+if __name__ == "__main__":
     from skfem import *
     from skfem.models import laplace, unit_load
 
@@ -12,6 +48,10 @@ if __name__ == '__main__':
     e = ElementTriP1()
 
     basis = Basis(m, e)
+    A = asm(laplace, basis)
+    f = asm(unit_load, basis)
+
+    # Penalty method
 
     fbases = [
         FacetBasis(m, e, facets='left'),
@@ -28,63 +68,35 @@ if __name__ == '__main__':
         jv = v1 - 2 * v2
         return 1. / 1e-2 * ju * jv
 
-    A = asm(laplace, basis)
     B = asm(penalty, fbases, fbases)
-    f = asm(unit_load, basis)
 
-    y = solve(*condense(A + B, f, D=basis.get_dofs({'top', 'bottom'}), x=basis.ones()))
+    y1 = solve(
+        *condense(A + B, f, D=basis.get_dofs({'top', 'bottom'}), x=basis.ones()))
 
-    basis.plot(y, colorbar=True, shading='gouraud', levels=5, ax=m.draw()).show()
+    basis.plot(y1, colorbar=True, shading='gouraud', levels=5).show()
 
+    # Master-Slave constraint
 
-def multipoint(Dx, *Ixs, g=None):
-    D,x = Dx
-    if x is None:
-        x = np.ones((D.N-len(D.flatten()),))
-    if g is None:
-        g = np.zeros(D.N)
-    shape = D.N, D.N-len(D.flatten())
-
-    D_rows = np.arange(D.N-len(D.flatten()))
-    D_columns = np.setdiff1d(np.arange(D.N), D)
-
-    result = csr_array(
-        (x, (D_columns, D_rows)), shape=shape
+    T1, g1, I1 = multipoint(
+        basis.get_dofs('left'),
+        (basis.get_dofs('right'), [2]*len(basis.get_dofs('right').flatten()))
     )
 
-    for Ix in Ixs:
-        I, x = Ix
-        result+= csr_array(
-            (x, (D.flatten(),[D_columns.tolist().index(i) for i in I.flatten()])), shape=shape
-        )
-
-    if len(np.nonzero(g[D_columns])[0]):
-        raise ValueError('g not correct, defines not removed DOFs')
-    return result, g, np.setdiff1d(np.arange(D.N), D)
-
-def combine(M1, M2):
-    T1, g1, I1 = M1
-    T2, g2, I2 = M2
-
-    return T1[:,np.isin(I1, I2)]@T2[np.intersect1d(I1,I2),:][:,np.isin(I2, I1)], g1 + g2
-
-def apply(A, f, T, g):
-    return T.T@A@T, T.T@(f-A@g)
-
-if __name__ == "__main__":
-    T1, g1, I1=multipoint((basis.get_dofs('left'), None), (basis.get_dofs('right'), [2]*len(basis.get_dofs('right').flatten())))
     g2 = basis.zeros()
     g2[basis.get_dofs('top')] = 1
-    T2, g2, I2=multipoint((basis.get_dofs('top'),None), g=g2)
+    T2, g2, I2 = multipoint(basis.get_dofs('top'), g=g2)
+
     g3 = basis.zeros()
     g3[basis.get_dofs('bottom')] = 1
-    T3, g3, I3=multipoint((basis.get_dofs('bottom') ,None), g=g3)
-    print(g2,g3)
+    T3, g3, I3 = multipoint(basis.get_dofs('bottom'), g=g3)
 
-    T, g = combine((T1,g1,I1),(T2,g2,I2))
-    T, g = combine((T,g,np.intersect1d(I1,I2)),(T3,g3,I3))
+    T, g = combine((T1, g1, I1), (T2, g2, I2))
+    T, g = combine((T, g, np.intersect1d(I1, I2)), (T3, g3, I3))
 
-    y = solve(*apply(A,f,T,g))
+    y2 = restore(solve(*apply(A, f, T, g)), T, g)
 
-    basis.plot(T@y+g, colorbar=True, shading='gouraud', levels=5, ax=m.draw()).show()
+    basis.plot(y2, colorbar=True, shading='gouraud', levels=5).show()
 
+    # Compare
+
+    assert np.all(np.isclose(y1, y2, atol=2e-2))
