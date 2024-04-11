a477591b87024fe994c841eeb9293b1733616de6
Jozef Vesely <vesely@gjh.sk>
2024-04-11 15:56:10 +0200

    doflocs for p0 elements should be in the center of gravity

--- skfem/element/element_tet/element_tet_p0.py
+++ skfem/element/element_tet/element_tet_p0.py
@@ -10,7 +10,7 @@ class ElementTetP0(ElementH1):
     interior_dofs = 1
     maxdeg = 0
     dofnames = ['u']
-    doflocs = np.array([[.5, .5, .5]])
+    doflocs = np.array([[.25, .25, .25]])
     refdom = RefTet
 
     def lbasis(self, X, i):
