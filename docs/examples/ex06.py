bd16209dc25737f63a593c0758873704cace0331
Tom Gustafsson <tom.gustafsson@iki.fi>
2024-06-15 14:33:40 +0300

    modernize ex06

--- docs/examples/ex06.py
+++ docs/examples/ex06.py
@@ -33,16 +33,15 @@ m = MeshQuad().refined(2)
 
 e1 = ElementQuad1()
 e = ElementQuad2()
-mapping = MappingIsoparametric(m, e1)
-ib = Basis(m, e, mapping, 4)
+basis = Basis(m, e, intorder=4)
 
-K = asm(laplace, ib)
+K = laplace.assemble(basis)
 
-f = asm(unit_load, ib)
+f = unit_load.assemble(basis)
 
-x = solve(*condense(K, f, D=ib.get_dofs()))
+x = solve(*condense(K, f, D=basis.get_dofs()))
 
-M, X = ib.refinterp(x, 3)
+M, X = basis.refinterp(x, 3)
 
 if __name__ == "__main__":
     from os.path import splitext
